0fe30fad4d982fed6552c6861bb3c74f9c64c8cf
Martin Apiwat <martin.apiwat@gmail.com>
2023-02-18 13:13:12 +0100

    confirmation page

--- klarna.js
+++ klarna.js
@@ -57,3 +57,15 @@ export async function createOrder(product) {
   console.log(data);
   return data;
 }
+
+export async function retrieveOrder(order_id) {
+  const auth = getKlarnaAuth();
+  const url =
+    "https://api.playground.klarna.com/checkout/v3/orders/" + order_id;
+
+  const response = await fetch(url, {
+    headers: { "Content-Type": "application/json", Authorization: auth },
+  });
+  const data = await response.json();
+  return data;
+}
